0bc09a62cc2611ed25c23e66e0748355a29ed4ce
yavuzproje <81007139+yavuzproje@users.noreply.github.com>
2021-03-20 10:13:38 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+for (let index = 0; index < 4; index++) {
+	
+}
+basic.showLeds(`
+    . # # # .
+    . # # # .
+    . . # . .
+    . . # . .
+    . # . # .
+    `)
+basic.showLeds(`
+    # . # . #
+    . # # # .
+    . . # . .
+    . . # . .
+    . # . # .
+    `)
+basic.showString("MERHABA BEN CİN ALİ")
+music.playMelody("D E E F F E D C ", 120)
+basic.forever(function () {
+	
+})
